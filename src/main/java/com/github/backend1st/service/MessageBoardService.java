d5a706171f180ad9a658f33e422aa2db7d1852dc
Hansol Shin <118977334+shinhansol1057@users.noreply.github.com>
2023-11-30 00:10:03 +0900

    Revert "Feature sihu"

--- src/main/java/com/github/backend1st/service/MessageBoardService.java
+++
@@ -1,9 +0,0 @@
-package com.github.backend1st.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class MessageBoardService {
-}

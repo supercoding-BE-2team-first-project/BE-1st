4891eca2978d32d3454fc31117eaf37215148c62
이충열 <chancho0518@gmail.com>
2023-12-10 04:08:33 +0900

    #18 commentDto 구현

---
+++ src/main/java/com/github/backend1st/web/dto/CommentDto.java
@@ -0,0 +1,22 @@
+package com.github.backend1st.web.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CommentDto {
+
+    private Integer commentId;
+    private String content;
+    private String createdAt;
+    private Integer userId;
+    private Integer postId;
+
+}
+
